127749d88cb53eb45376efbf421b0a2e2fea07e0
kuisiba <kuisiba@gmail.com>
2020-01-04 15:06:06 +0900

    03

---
+++ chapter1/03.py
@@ -0,0 +1,11 @@
+import re
+
+s = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+ss = re.sub(r'[,.]', '', s)
+sss = map(lambda x:len(x), ss.split())
+ans = ""
+for i in sss:
+    ans += str(i)
+
+print(ans)
