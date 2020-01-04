df9414ac6687084963e3f774fb88bb362ba6fa47
kuisiba <kuisiba@gmail.com>
2020-01-04 17:12:43 +0900

    format 12

--- chapter2/12.py
+++ chapter2/12.py
@@ -7,7 +7,7 @@ col2 = []
 for i, x in enumerate(s.split()):
     if i % 4 == 0:
         col1.append(x)
-    elif i%4 == 1:
+    elif i % 4 == 1:
         col2.append(x)
 
 f = open('./chapter2/col1.txt', mode='w')
