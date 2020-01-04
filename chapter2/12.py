32c849b115b647bdde774bae61690ee8b2eab687
kuisiba <kuisiba@gmail.com>
2020-01-04 17:08:29 +0900

    12

---
+++ chapter2/12.py
@@ -0,0 +1,19 @@
+f = open('./chapter2/hightemp.txt')
+s = f.read()
+f.close()
+
+col1 = []
+col2 = []
+for i, x in enumerate(s.split()):
+    if i % 4 == 0:
+        col1.append(x)
+    elif i%4 == 1:
+        col2.append(x)
+
+f = open('./chapter2/col1.txt', mode='w')
+f.write('\n'.join(col1))
+f.close()
+
+f = open('./chapter2/col2.txt', mode='w')
+f.write('\n'.join(col2))
+f.close()
---
+++ chapter2/12.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+cut -f 1 chapter2/hightemp.txt
+cut -f 2 chapter2/hightemp.txt
