a03a12323fb5bcbfba011f5ec21959cdbc575308
kuisiba <kuisiba@gmail.com>
2020-01-04 15:50:29 +0900

    05

---
+++ chapter1/05.py
@@ -0,0 +1,14 @@
+def ngram(s, n):
+    arr = []
+    for i in range(len(s) - n + 1):
+        arr.append(s[i:i+n])
+    return arr
+
+
+s = "I am an NLPer"
+
+words = s.split()
+print(ngram(words, 2))
+
+chars = list(s)
+print(ngram(chars, 2))
