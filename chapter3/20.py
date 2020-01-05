a324ab8f608f8d8ba37f04ebfdf082f3a108f375
kuisiba <kuisiba@gmail.com>
2020-01-05 17:23:48 +0900

    fix 20

--- chapter3/20.py
+++ chapter3/20.py
@@ -5,6 +5,7 @@ f = gzip.open('./chapter3/jawiki-country.json.gz', mode='rt')
 for l in f:
     data = json.loads(l)
     if data['title'] == 'イギリス':
-        data['text'] = data['text'].replace('\n', '')
-        fw = open('./chapter3/igirisu.json', 'w')
-        json.dump(data, fw, ensure_ascii=False)
+        print(data['text'])
+        fw = open('./chapter3/igirisu.txt', 'w')
+        fw.write(data['text'])
+        fw.close()
