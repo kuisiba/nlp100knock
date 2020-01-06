5a9eaec1446b3e4ccbf2f11d769c2cef133eaf61
kuisiba <kuisiba@gmail.com>
2020-01-06 16:01:30 +0900

    32

---
+++ chapter4/32.py
@@ -0,0 +1,24 @@
+import MeCab
+import re
+
+mecab = MeCab.Tagger('-MeCab')
+
+f = open('./chapter4/neko.txt.mecab')
+neko = f.readlines()
+f.close()
+
+arr = []
+for i in range(len(neko)):
+    matched = re.match(r'(.*)\t(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*)', neko[i])
+    sentence = {}
+    if matched:
+        sentence['surface'] = matched.group(1)
+        sentence['base'] = matched.group(8)
+        sentence['pos'] = matched.group(2)
+        sentence['pos1'] = matched.group(3)
+    if '動詞' in sentence.values():
+        #arr.append(sentence['base'])
+        arr.append(sentence['base'])
+
+for i in arr:
+    print(i)
