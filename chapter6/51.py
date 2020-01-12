30c9854b03aa89162e82fe1eb5d528c906aee112
kuisiba <kuisiba@gmail.com>
2020-01-12 15:45:00 +0900

    51

---
+++ chapter6/51.py
@@ -0,0 +1,29 @@
+import re
+
+f = open('chapter6/nlp.txt')
+lines = f.readlines()
+f.close()
+
+pattern = re.compile(r'(^.*?[\.|;|:|\?|\!])\s([A-Z].*)')
+
+arr = []
+for x in lines:
+    x = x.strip()
+    while len(x) > 0:
+        matched = pattern.match(x)
+        if matched:
+            arr.append(matched.group(1))
+            x = matched.group(2)
+        else:
+            arr.append(x)
+            x = ''
+
+arr2 = []
+for element in arr:
+    element = element.split(' ')
+    for element2 in element:
+        arr2.append(element2)
+    arr2.append("")
+
+for element in arr2:
+    print(element)
